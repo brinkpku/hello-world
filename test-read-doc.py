b814da2a5503d0ba65df67361291d28930ecac62
brink <brinkymy@gmail.com>
2019-06-17 09:36:07 +0800

    Create test-read-doc.py
    to test ReadtheDoc

---
+++ test-read-doc.py
@@ -0,0 +1,33 @@
+# -*- coding:utf8 -*-
+
+'''
+Test Read the Doc. This module doc.
+date: 2019-06-17
+author: brink
+'''
+
+import os
+
+class Test(object):
+    '''
+    This is class doc.
+    '''
+    def __init__(self, para):
+        '''
+        This is constructor method doc.
+        para: para description.
+        '''
+        self._para = para
+    
+    def _test():
+        '''
+        This is instance method doc.
+        '''
+        pass
+        
+def test(para):
+    '''
+    This is module method doc.
+    para: int, blabla
+    '''
+    pass
